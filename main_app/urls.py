97f85ee55e2a9298cedad0ed89cb0be312241eb8
shaybay <pacificconnect310@gmail.com>
2022-04-25 18:45:23 -0700

    detail functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path("boards/", views.boards_index, name='index')
+    path('boards/', views.boards_index, name='index'),
+    path('boards/<int:board_id>/', views.boards_details, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# this is a temporary replacement for a database
-class Board:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, brand, model, description, age):
-    self.brand = brand
-    self.model = model
-    self.description = description
-    self.age = age
+from .models import Board
 
-boards = [
-  Board('Burton', 'slayer', 'foul little demon', 3),
-  Board('Lamar', 'slickBack', 'Degenerate myalopathy', 2),
-  Board('k2', 'splice', 'really really not that bad', 6)
-]
+
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>Hello</h1>')
@@ -20,5 +10,13 @@ def home(request):
 def about(request):
     return render (request, 'about.html')
 
+  
 def boards_index(request):
-    return render(request, 'boards/index.html', {'boards': boards})
+  boards = Board.objects.all()
+  return render(request, 'boards/index.html',{'boards':boards})
+
+def boards_details(request, board_id):
+  board=Board.objects.get(id=board_id)
+  return render(request, 'boards/detail.html', {'board': board})
+
+
